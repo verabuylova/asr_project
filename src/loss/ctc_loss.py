d21511c00fe0521f776621061b6f93ae9111842c
Вера <vera@Vera--Notebook.local>
2024-10-10 19:31:24 +0300

    Add onebatchtest config

--- src/loss/ctc_loss.py
+++ src/loss/ctc_loss.py
@@ -4,6 +4,9 @@ from torch.nn import CTCLoss
 
 
 class CTCLossWrapper(CTCLoss):
+    def __init__(self, blank = 0, reduction = 'mean', zero_infinity = True):
+        super().__init__(blank, reduction, zero_infinity)
+
     def forward(
         self, log_probs, log_probs_length, text_encoded, text_encoded_length, **batch
     ) -> Tensor:
