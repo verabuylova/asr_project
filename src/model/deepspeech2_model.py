ce8db681ab855e8ecf62f938ea9861f9e59df5d0
Вера <vera@Vera--Notebook.Dlink>
2024-10-11 13:29:40 +0300

    Change code structure

--- src/model/deepspeech2_model.py
+++ src/model/deepspeech2_model.py
@@ -2,8 +2,24 @@ import torch
 from torch import nn
 from torch.nn import Sequential, BatchNorm1d
 
-class DeepSpeech2Model(nn.Module):
+class GRUWithBatchNorm(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(GRUWithBatchNorm, self).__init__()
+        self.gru = nn.GRU(input_size, hidden_size, bidirectional=True)
+        self.batchnorm = BatchNorm1d(hidden_size)
 
+    def forward(self, x):
+        x, _ = self.gru(x)
+        x = x.view(x.shape[0], x.shape[1], 2, -1).sum(2)
+        T, N = x.shape[0], x.shape[1] 
+        x = x.view(T * N, -1)
+        x = self.batchnorm(x) 
+        # [T, N, hidden_size]
+        x = x.view(T, N, -1)
+        return x
+
+
+class DeepSpeech2Model(nn.Module):
     def __init__(self, n_feats, fc_hidden, n_tokens):
         """
         Args:
@@ -14,50 +30,43 @@ class DeepSpeech2Model(nn.Module):
         super().__init__()
 
         self.conv = Sequential(
-            nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = (41, 11), padding = (20, 5), stride = (2, 2)),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(41, 11), padding=(20, 5), stride=(2, 2)),
             nn.BatchNorm2d(32),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (21, 11), padding = (10, 5), stride = (2, 1)),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(21, 11), padding=(10, 5), stride=(2, 1)),
             nn.BatchNorm2d(32),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels = 32, out_channels = 96, kernel_size = (21, 11), padding = (10, 5), stride = (2, 1)),
+            nn.Conv2d(in_channels=32, out_channels=96, kernel_size=(21, 11), padding=(10, 5), stride=(2, 1)),
             nn.BatchNorm2d(96),
             nn.ReLU()
         )
 
         input_size = ((((((n_feats + 40 - 41) // 2 + 1) + 20 - 21) // 2 + 1) + 20 - 21) // 2 + 1) * 96
 
-        self.grus = nn.ModuleList([nn.GRU(input_size, fc_hidden, bidirectional = True)] + 
-                                  [nn.GRU(fc_hidden, fc_hidden, bidirectional = True) for _ in range(4)])
-        
-        self.batchnorms = nn.ModuleList([BatchNorm1d(fc_hidden) for _ in range(5)])
+        self.grus = nn.ModuleList([GRUWithBatchNorm(input_size, fc_hidden)] + 
+                                  [GRUWithBatchNorm(fc_hidden, fc_hidden) for _ in range(4)])
 
         self.fc = nn.Linear(fc_hidden, n_tokens)
 
     def forward(self, spectrogram, spectrogram_length, **batch):
-        x = self.conv(spectrogram.unsqueeze(1))
+        x = self.conv(spectrogram.unsqueeze(1))  # [N, 1, F, T]
         N, C, F, T = x.shape
-        x = x.view(N, C * F, T)
-        x = x.transpose(1, 2) 
-        x = x.transpose(0, 1).contiguous()
+        x = x.view(N, C * F, T)  # [N, C*F, T]
+        x = x.transpose(1, 2)  # [N, T, C*F]
+        x = x.transpose(0, 1).contiguous()  # [T, N, C*F]
 
-        for gru, batchnorm in zip(self.grus, self.batchnorms):
-            x, h = gru(x)
-            x = x.view(x.shape[0], x.shape[1], 2, -1).sum(2)
-            T_gru, N_gru = x.shape[0], x.shape[1]
-            x = x.view((T_gru * N_gru, -1))
-            x = batchnorm(x)
-            x = x.view((T_gru, N_gru, -1)).contiguous()
+        for gru_with_bn in self.grus:
+            x = gru_with_bn(x)
 
         T, N = x.shape[0], x.shape[1]
         x = x.view(T * N, -1) 
         x = self.fc(x)
         x = x.view(T, N, -1)
-        x = x.transpose(0, 1)
-        
-        return {'log_probs': nn.functional.log_softmax(x, dim = -1), 'log_probs_length': self.transform_input_lengths(spectrogram_length)}
+        x = x.transpose(0, 1)  # [N, T, n_tokens]
+
+        return {'log_probs': nn.functional.log_softmax(x, dim=-1), 'log_probs_length': self.transform_input_lengths(spectrogram_length)}
 
     def transform_input_lengths(self, input_lengths):
         return torch.zeros_like(input_lengths).fill_((((((input_lengths.max() + 2 * 5 - 11) // 2 + 1) + 2 * 5 - 11) // 2 + 1) + 2 * 5 - 11) + 1)
@@ -67,9 +76,7 @@ class DeepSpeech2Model(nn.Module):
         Model prints with the number of parameters.
         """
         all_parameters = sum([p.numel() for p in self.parameters()])
-        trainable_parameters = sum(
-            [p.numel() for p in self.parameters() if p.requires_grad]
-        )
+        trainable_parameters = sum([p.numel() for p in self.parameters() if p.requires_grad])
 
         result_info = super().__str__()
         result_info = result_info + f"\nAll parameters: {all_parameters}"
