94864582f17fe3dbf837871d9989b4a58d9e3923
Вера <vera@Vera--Notebook.local>
2024-10-13 08:13:54 +0300

    Separate files

--- src/transforms/spec_augs/__init__.py
+++ src/transforms/spec_augs/__init__.py
@@ -1,4 +1,5 @@
-from src.transforms.spec_augs.masking import TimeMasking, FrequencyMasking
+from asr.src.transforms.spec_augs.time_masking import TimeMasking
+from asr.src.transforms.spec_augs.freq_masking import FrequencyMasking
 
 all = [
     "TimeMasking",
